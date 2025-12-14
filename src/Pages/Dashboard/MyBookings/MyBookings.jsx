56f011c7a7dffcb4a9ddd268ed9a558fdf967250
jamal-uddin-asif <asifzehendmg@gmail.com>
2025-12-15 01:45:14 +0600

    Sort by date and sort by status functionality added in My booking page

--- src/Pages/Dashboard/MyBookings/MyBookings.jsx
+++ src/Pages/Dashboard/MyBookings/MyBookings.jsx
@@ -9,7 +9,9 @@ import Swal from "sweetalert2";
 import Container from "../../../Components/Shared/Container";
 import Heading from "../../../Components/Shared/Heading";
 import { Link } from "react-router";
-
+import { Field, Label, Select } from "@headlessui/react";
+import { IoChevronDownCircleOutline } from "react-icons/io5";
+import clsx from "clsx";
 const MyBookings = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
@@ -17,7 +19,10 @@ const MyBookings = () => {
   const [totalBookings, setTotalBookings] = useState(0);
   const [totalPage, setTotalPage] = useState(0);
   const [currentPage, setCurrentPage] = useState(0);
-
+  const [sort, setSort] = useState("");
+  const [status, setStatus] = useState("");
+  console.log(status)
+  console.log(sort);
   const limit = 5;
 
   useEffect(() => {
@@ -29,12 +34,12 @@ const MyBookings = () => {
   }, [totalBookings, axiosSecure, user]);
 
   const { data: bookings = [], refetch } = useQuery({
-    queryKey: ["bookings",currentPage, user.email],
+    queryKey: ["bookings", currentPage, user.email, sort, status],
     queryFn: async () => {
       const res = await axiosSecure(
         `/bookings?email=${user.email}&limit=${limit}&skip=${
           currentPage * limit
-        }`
+        }&sort=${sort}&status=${status}`
       );
 
       return res.data.bookings;
@@ -46,7 +51,6 @@ const MyBookings = () => {
     console.log("inside func", i);
     refetch();
   };
-  console.log("outside func", currentPage);
 
   const handleDelete = (booking) => {
     Swal.fire({
@@ -86,8 +90,13 @@ const MyBookings = () => {
     });
   };
 
+  const handleSortStatus = e =>{
+    setStatus(e)
+    refetch()
+  }
+
   return (
-    <div>
+    <div className="">
       <Heading
         className={"mb-10"}
         Heading={"Booking & Status Overview"}
@@ -96,6 +105,69 @@ const MyBookings = () => {
         }
       ></Heading>
       <Container>
+        <div className="shadow bg-secondary/20 mb-10 rounded-2xl py-5 ">
+        <div>
+          <div className="flex items-center">
+
+            {/* date sorting  */}
+
+            <div className="w-full max-w-[200px] px-4">
+              <Field>
+                {/* <Description className="text-sm/6 text-white/50">This will be visible to clients on the project.</Description> */}
+                <div className="relative">
+                  <Select
+                    onChange={(e) => setSort(e.target.value)}
+                    className={clsx(
+                      "mt-3 block w-full appearance-none rounded-lg border-none bg-secondary text-white shadow px-3 py-1.5 text-sm/6 ",
+                      "focus:not-data-focus:outline-none data-focus:outline-2 data-focus:-outline-offset-2 data-focus:outline-white"
+                      // Make the text of each option black on Windows
+                    )}
+                  >
+                    <option value="Newest-first">Newest first</option>
+                    <option value="Oldest-first">Oldest first</option>
+                  </Select>
+                  <IoChevronDownCircleOutline
+                    color="white"
+                    className="group pointer-events-none absolute top-2.5 right-2.5 size-4 fill-white/60 "
+                    aria-hidden="true"
+                  />
+                </div>
+              </Field>
+            </div>
+
+            {/* Status sorting  */}
+            <div className="w-full max-w-[250px] px-4">
+              <Field>
+                {/* <Description className="text-sm/6 text-white/50">This will be visible to clients on the project.</Description> */}
+                <div className="relative space-x-2.5">
+                  <Select
+                    onChange={(e) => handleSortStatus(e.target.value)}
+                    className={clsx(
+                      "mt-3  block w-full appearance-none rounded-lg border-none bg-secondary text-white shadow px-3 py-1.5 text-sm/6 ",
+                      "focus:not-data-focus:outline-none data-focus:outline-2 data-focus:-outline-offset-2 data-focus:outline-white"
+                      // Make the text of each option black on Windows
+                    )}
+                  >
+                  <option value="Pending">Pending</option>
+                  <option value="Assigned">Assigned</option>
+                  <option value="Planning Phase">Planning Phase</option>
+                  <option value="Materials Prepared">Materials Prepared</option>
+                  <option value="On the Way to Venue">On the Way to Venue</option>
+                  <option value="Setup in Progress">Setup in Progress</option>
+                  <option value="Completed">Completed</option>
+                  </Select>
+                  <IoChevronDownCircleOutline
+                    color="white"
+                    className="group  pointer-events-none absolute top-1/2 -translate-y-1/2 right-2.5 size-4 fill-white/60 "
+                    aria-hidden="true"
+                  />
+                </div>
+              </Field>
+            </div>
+            
+          </div>
+        </div>
+        </div>
         <div className="overflow-x-auto">
           <table className="table table-zebra">
             {/* head */}
@@ -105,6 +177,7 @@ const MyBookings = () => {
                 <th className="">Service</th>
                 <th className="">Cost</th>
                 <th>Feet</th>
+                <th>Booking Date</th>
                 <th>Tracking Id</th>
                 <th>Service Status</th>
                 <th>Pay</th>
@@ -133,7 +206,17 @@ const MyBookings = () => {
 
                   <td>{booking.cost}</td>
                   <td>{booking.feet}</td>
-                  <td> <Link to={`/dashboard/trackings/${booking.trackingId}`}> {booking.trackingId}</Link> </td>
+                  <td>{booking.bookingDate}</td>
+                  <td>
+                    {" "}
+                    <Link
+                      className="link-hover"
+                      to={`/dashboard/trackings/${booking.trackingId}`}
+                    >
+                      {" "}
+                      {booking.trackingId}
+                    </Link>{" "}
+                  </td>
                   <td>{booking.serviceStatus}</td>
                   <td className="">
                     {booking?.paymentStatus === "Paid" ? (
@@ -167,7 +250,9 @@ const MyBookings = () => {
           <div className="flex justify-center py-6 gap-3">
             {[...Array(totalPage).keys()].map((i) => (
               <button
-              className={`${i === currentPage ? 'btn btn-secondary ' : 'btn'}`}
+                className={`${
+                  i === currentPage ? "btn btn-secondary " : "btn"
+                }`}
                 onClick={() => handlePagination(i)}
                 key={i}
               >
